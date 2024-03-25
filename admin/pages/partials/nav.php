d0b62bbcec95892f24ad58d7ba1ac83a8bbd3889
John Mark <johnmark.cuyos@evsu.edu.ph>
2024-03-25 18:22:11 +0800

    css updates

--- admin/pages/partials/nav.php
+++ admin/pages/partials/nav.php
@@ -1,25 +1,25 @@
-<nav id="sidebar" class="bg-light">
-    <div class="sidebar-header text-center">
+<nav id="sidebar" class="bg-light border-right">
+    <div class="sidebar-header text-center border-bottom pb-3">
         <button type="button" id="sidebarCollapse" class="btn btn-light">
             <i class="bi bi-list"></i>
         </button>
-        <h1><a href="index.php" class="logo">ADMIN</a></h1>
+        <h1><a href="index.php" class="logo text-dark">ADMIN</a></h1>
     </div>
-    <ul class="list-unstyled components mt-4">
-        <li class="active">
-            <a href="index.php"><i class="bi bi-person-fill mr-3"></i> DASHBOARD</a>
+    <ul class="list-unstyled components ">
+        <li class="border-bottom pb-2">
+            <a href="index.php" class="d-block py-2 text-dark"><i class="bi bi-person-fill mr-3"></i> DASHBOARD</a>
         </li>
-        <li>
-            <a href="inventory.php"><i class="bi bi-info-circle-fill mr-3"></i> INVENTORY</a>
+        <li class="border-bottom pb-2">
+            <a href="inventory.php" class="d-block py-2 text-dark"><i class="bi bi-info-circle-fill mr-3"></i> INVENTORY</a>
         </li>
-        <li>
-            <a href="#"><i class="bi bi-info-circle-fill mr-3"></i> ORDERS</a>
+        <li class="border-bottom pb-2">
+            <a href="#" class="d-block py-2 text-dark"><i class="bi bi-info-circle-fill mr-3"></i> ORDERS</a>
         </li>
-        <li>
-            <a href="#"><i class="bi bi-info-circle-fill mr-3"></i> SALES HISTORY</a>
+        <li class="border-bottom pb-2">
+            <a href="#" class="d-block py-2 text-dark"><i class="bi bi-info-circle-fill mr-3"></i> SALES HISTORY</a>
         </li>
-        <li>
-            <a href="#"><i class="bi bi-gear-fill mr-3"></i> SETTINGS</a>
+        <li class="border-bottom pb-2">
+            <a href="#" class="d-block py-2 text-dark"><i class="bi bi-gear-fill mr-3"></i> SETTINGS</a>
         </li>
     </ul>
     <div class="text-center mt-4">
