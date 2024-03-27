8cbf5da4bdd60cb2fdb6d5e2076536b1c168b74f
John Mark <johnmark.cuyos@evsu.edu.ph>
2024-03-27 10:15:19 +0800

    changes

--- admin/pages/login.php
+++ admin/pages/login.php
@@ -8,32 +8,41 @@ if (!isset($_SESSION['admin'])) {
 ?>
 
 <div class="container py-5">
-    <div class="row justify-content-center align-items-center">
+    <div class="row justify-content-center">
         <div class="col-md-6">
-            <div class="card">
-                <div class="card-body">
-                    <h1 class="card-title text-center">LOGIN</h1>
-                    <form action="#" method="post">
-                        <div class="mb-3">
-                            <label for="employeeid" class="form-label">Employee ID</label>
-                            <input type="text" name="employeeid" id="employeeid" class="form-control" required autocomplete="on">
+            <div class="card p-4">
+                <div class="row">
+                    <div class="col-md-6 d-flex align-items-center justify-content-center">
+                        <img src="../images/237170717_10159380668691838_867040353180657182_n.png" alt="login" class="img-fluid">
+                    </div>
+                    <div class="col-md-6">
+                        <div class="card-body">
+                            <h1 class="card-title text-center">LOGIN</h1>
+                            <form action="#" method="post">
+                                <div class="mb-3">
+                                    <label for="employeeid" class="form-label">Employee ID</label>
+                                    <input type="text" name="employeeid" id="employeeid" class="form-control" required autocomplete="on">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input type="password" name="password" id="password" class="form-control" required autocomplete="on">
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary" onclick="login(event)">Login</button>
+                                </div>
+                                <div class="form-group mt-3 text-center">
+                                    <a href="register.php">Don't have an account? Register</a>
+                                </div>
+                            </form>
                         </div>
-                        <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" name="password" id="password" class="form-control" required autocomplete="on">
-                        </div>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-primary" onclick="login(event)">Login</button>
-                        </div>
-                        <div class="form-group mt-3 text-center">
-                            <a href="register.php"> Don't have an account? Register</a>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+
 
 <?php
 } else {
--- pages/login.php
+++ pages/login.php
@@ -8,42 +8,52 @@ if (!isset($_SESSION['user'])) {
 ?>
 
 
-    <div class="container py-5">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header text-center">
-                        <h1>MUST LOGIN FIRST!</h1>
+<div class="container py-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <div class="card p-4">
+                <div class="row">
+                    <div class="col-md-6 d-flex align-items-center justify-content-center">
+                        <img src="../images/237170717_10159380668691838_867040353180657182_n.png" alt="login" class="img-fluid">
                     </div>
-                    <div class="card-body">
-                        <form action="#" method="post">
-                            <div class="form-group mt-3">
-                                <label for="studentid" class="form-label">STUDENT ID</label>
-                                <input type="text" class="form-control" id="studentid" name="studentid" autocomplete="on" required>
-                            </div>
-                            <div class="form-group mt-3">
-                                <label for="password" class="form-label">PASSWORD</label>
-                                <input type="password" class="form-control" id="password" name="password" autocomplete="on" required>
-                            </div>
-                            <div class="form-group text-center mt-3">
-                                <button type="submit" class="btn btn-primary" onclick="login(event)">LOGIN</button>
-                            </div>
-                            <div class="form-group text-center mt-3">
-                                <a href="register.php">Don't have an account? REGISTER</a>
-                            </div>
-                        </form>
+                    <div class="col-md-6">
+                        <div class="card-body">
+                        <h1 class="card-title text-center">LOGIN</h1>
+                            <form action="#" method="post">
+                                <div class="form-group mt-3">
+                                    <label for="studentid" class="form-label">STUDENT ID</label>
+                                    <input type="text" class="form-control" id="studentid" name="studentid" autocomplete="on" required>
+                                </div>
+                                <div class="form-group mt-3">
+                                    <label for="password" class="form-label">PASSWORD</label>
+                                    <input type="password" class="form-control" id="password" name="password" autocomplete="on" required>
+                                </div>
+                                <div class="form-group text-center mt-4">
+                                    <button type="submit" class="btn btn-primary btn-block" onclick="login(event)">LOGIN</button>
+                                </div>
+                                <div class="form-group text-center mt-3">
+                                    <a href="register.php">Don't have an account? REGISTER</a>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 
-<?php
+
+
+
+
+
+    <?php
 } else {
     header("Location: index.php");
 }
 
 
 require 'partials/footer.php';
-?>
+    ?>
